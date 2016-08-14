fc16741af28149ebec85e46be0a5f823117db862
Mark Fajet <markfajet@gmail.com>
2016-08-14 15:21:53 -0400

    Made types explicit

--- haskell-solutions/Problem6.hs
+++ haskell-solutions/Problem6.hs
@@ -1,8 +1,12 @@
 module Problem6(answer,sumOfSquares,squareOfSum, answerAsWhole) where
+  answerAsWhole :: Double -> Integer
   answerAsWhole x = round $ answer x
 
+  answer :: Double -> Double
   answer x = (squareOfSum x) - (sumOfSquares x)
 -- sum of squares of 1 - n is given by n*(n+1)*(2*n+1) / 6
+  sumOfSquares :: Double -> Double
   sumOfSquares n = n*(n+1)*(2*n+1) / 6
 -- sum of digits 1-n is given by n*(n+1)/2
+  squareOfSum :: Double -> Double
   squareOfSum n = (n*(n+1)/2)^2
