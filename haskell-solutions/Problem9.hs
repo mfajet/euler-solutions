a633c5f1344d9de31ebe6055ab385a3c12175c1a
Mark Fajet <markfajet@gmail.com>
2016-08-17 22:35:05 -0400

    Refined it so it returns actual answer.

--- haskell-solutions/Problem9.hs
+++ haskell-solutions/Problem9.hs
@@ -1,3 +1,3 @@
 module Problem9(findTriple) where
-  findTriple :: Integer -> [[Integer]]
-  findTriple s = [[a,b,c] | a <- [1..s], b <- [1..s], let c =ceiling $ sqrt$ fromIntegral (a^2 + b^2), a<b,a^2+b^2==c^2,  a + b + c==s]
+  findTriple :: Integer -> Integer
+  findTriple s = product$ head$ [[a,b,c] | a <- [1..s], b <- [1..s], let c =ceiling $ sqrt$ fromIntegral (a^2 + b^2), a<b,a^2+b^2==c^2,  a + b + c==s]
