6d1b3d3ee230a51a684b5bb20d10836a4728c0af
Mark Fajet <markfajet@gmail.com>
2017-01-08 19:21:29 -0500

    Problem 31 abstracted to work for various coin collections by passing array of available coins in descending order.

--- haskell-solutions/Problem31.hs
+++ haskell-solutions/Problem31.hs
@@ -5,23 +5,5 @@ makeChange n (x:y:coins)
     | n - x >=0 = makeChange (n - x) (x:y:coins) + makeChange (n) (y:coins)
     | otherwise =  makeChange (n) (y:coins)
 
-nextCoin :: Int -> Int
-nextCoin coin
-    | coin == 200 = 100
-    | coin == 100 = 50
-    | coin == 50 = 20
-    | coin == 20 = 10
-    | coin == 10 = 5
-    | coin == 5 = 2
-    | coin == 2 = 1
-    | otherwise = 0
-
 main = do
     print $ makeChange 200 [200,100,50,20,10,5,2,1]
-
-
-
-    -- | coin == 25 = 10
-    -- | coin == 10 = 5
-    -- | coin == 5 = 1
-    -- | otherwise = 0
